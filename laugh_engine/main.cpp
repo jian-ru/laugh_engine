e84712ccdf7f585e22108999e610262683821806
jian-ru <jianru@seas.upenn.edu>
2016-11-21 16:14:16 -0500

    demo

--- laugh_engine/main.cpp
+++ laugh_engine/main.cpp
@@ -29,8 +29,8 @@
 const int WIDTH = 800;
 const int HEIGHT = 600;
 
-const std::string MODEL_PATH = "../models/chalet.obj";
-const std::string TEXTURE_PATH = "../textures/chalet.jpg";
+const std::string MODEL_PATH = "../models/dilpoh.obj";
+const std::string TEXTURE_PATH = "../textures/dilpoh.jpg";
 
 const std::vector<const char*> validationLayers =
 {
