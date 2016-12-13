9c6bed8c8376d4a8ccec5fb20c34f123a1985bad
jian-ru <jianru@seas.upenn.edu>
2016-12-13 00:01:56 -0500

    demo

--- laugh_engine/vbase.h
+++ laugh_engine/vbase.h
@@ -20,11 +20,6 @@
 #include "vtextoverlay.h"
 #include "vmesh.h"
 
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
-#define TINYOBJLOADER_IMPLEMENTATION
-#include "tiny_obj_loader.h"
-
 
 enum DisplayMode
 {
