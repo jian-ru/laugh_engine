dd2d1df7f98b485e68898a7f0db9fa729207ae36
jian-ru <jianru@seas.upenn.edu>
2016-12-24 16:59:45 -0500

    render pass and set layout

--- laugh_engine/VManager.h
+++ laugh_engine/VManager.h
@@ -27,6 +27,101 @@ namespace rj
 			std::vector<VkSubpassDescription> subpassDescs;
 			std::vector<VkSubpassDependency> subpassDependencies;
 		};
+
+		struct DescriptorSetLayoutCreateInfo
+		{
+			std::vector<VkDescriptorSetLayoutBinding> bindings;
+		};
+
+		struct DefaultGraphicsPipelineCreateInfo
+		{
+			VkPipelineLayoutCreateInfo pipelineLayoutInfo;
+
+			VkPipelineVertexInputStateCreateInfo vertexInputInfo;
+			VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo;
+			VkPipelineViewportStateCreateInfo viewportStateInfo;
+			VkPipelineRasterizationStateCreateInfo rasterizerInfo;
+			VkPipelineMultisampleStateCreateInfo multisamplingInfo;
+			VkPipelineDepthStencilStateCreateInfo depthStencilInfo;
+			std::vector<VkPipelineColorBlendAttachmentState> colorBlendAttachmentStates;
+			VkPipelineColorBlendStateCreateInfo colorBlendInfo;
+			VkPipelineDynamicStateCreateInfo dynamicStateInfo;
+			std::vector<VkPushConstantRange> pushConstantRanges;
+
+			VkGraphicsPipelineCreateInfo pipelineInfo;
+
+			VDeleter<VkShaderModule> vertShaderModule;
+			VDeleter<VkShaderModule> geomShaderModule;
+			VDeleter<VkShaderModule> fragShaderModule;
+
+			std::vector<VkPipelineShaderStageCreateInfo> shaderStages;
+
+			DefaultGraphicsPipelineCreateInfo(const VDeleter<VkDevice> &device)
+				:
+				vertShaderModule{ device, vkDestroyShaderModule },
+				geomShaderModule{ device, vkDestroyShaderModule },
+				fragShaderModule{ device, vkDestroyShaderModule }
+			{
+				pipelineLayoutInfo = {};
+				vertexInputInfo = {};
+				inputAssemblyInfo = {};
+				viewportStateInfo = {};
+				rasterizerInfo = {};
+				multisamplingInfo = {};
+				depthStencilInfo = {};
+				colorBlendAttachmentStates = {};
+				colorBlendInfo = {};
+				dynamicStateInfo = {};
+				pipelineInfo = {};
+
+				vertexInputInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
+
+				inputAssemblyInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
+				inputAssemblyInfo.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
+
+				viewportStateInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
+				viewportStateInfo.viewportCount = 1;
+				viewportStateInfo.scissorCount = 1;
+
+				rasterizerInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
+				rasterizerInfo.polygonMode = VK_POLYGON_MODE_FILL;
+				rasterizerInfo.lineWidth = 1.0f;
+				rasterizerInfo.cullMode = VK_CULL_MODE_BACK_BIT;
+				rasterizerInfo.frontFace = VK_FRONT_FACE_CLOCKWISE;
+
+				multisamplingInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
+				multisamplingInfo.rasterizationSamples = VK_SAMPLE_COUNT_1_BIT;
+
+				depthStencilInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_DEPTH_STENCIL_STATE_CREATE_INFO;
+				depthStencilInfo.depthTestEnable = VK_TRUE;
+				depthStencilInfo.depthWriteEnable = VK_TRUE;
+				depthStencilInfo.depthCompareOp = VK_COMPARE_OP_LESS_OR_EQUAL;
+
+				colorBlendAttachmentStates.resize(1);
+				colorBlendAttachmentStates[0].colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT;
+				colorBlendAttachmentStates[0].blendEnable = VK_FALSE;
+
+				colorBlendInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
+				colorBlendInfo.attachmentCount = 1;
+				colorBlendInfo.pAttachments = colorBlendAttachmentStates.data();
+
+				dynamicStateInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_DYNAMIC_STATE_CREATE_INFO;
+
+				pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+
+				pipelineInfo.sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO;
+				pipelineInfo.stageCount = static_cast<uint32_t>(shaderStages.size());
+				pipelineInfo.pStages = shaderStages.data();
+				pipelineInfo.pVertexInputState = &vertexInputInfo;
+				pipelineInfo.pInputAssemblyState = &inputAssemblyInfo;
+				pipelineInfo.pViewportState = &viewportStateInfo;
+				pipelineInfo.pRasterizationState = &rasterizerInfo;
+				pipelineInfo.pMultisampleState = &multisamplingInfo;
+				pipelineInfo.pDepthStencilState = &depthStencilInfo;
+				pipelineInfo.pColorBlendState = &colorBlendInfo;
+				pipelineInfo.pDynamicState = nullptr;
+			}
+		};
 	}
 
 	using namespace helper_functions;
@@ -49,11 +144,11 @@ namespace rj
 		}
 
 		// --- Render pass creation ---
-		void beginCreateRenderPass(const std::string &name)
+		void beginCreateRenderPass()
 		{
 			m_curRenderPassInfo = {};
-			m_curRenderPassName = name;
-			m_renderPasses[name] = VDeleter<VkRenderPass>{ m_device, vkDestroyRenderPass };
+			m_curRenderPassName = static_cast<uint32_t>(m_renderPasses.size());
+			m_renderPasses[m_curRenderPassName] = VDeleter<VkRenderPass>{ m_device, vkDestroyRenderPass };
 		}
 
 		void renderPassAddAttachment(VkFormat format,
@@ -181,7 +276,7 @@ namespace rj
 			dependency.dstAccessMask = dstAccessFlags;
 		}
 
-		void endCreateRenderPass()
+		uint32_t endCreateRenderPass()
 		{
 			VkRenderPassCreateInfo renderPassInfo = {};
 			renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
@@ -196,8 +291,49 @@ namespace rj
 			{
 				throw std::runtime_error("failed to create render pass!");
 			}
+
+			m_curRenderPassInfo = {};
+			return m_curRenderPassName;
 		}
 		// --- Render pass creation ---
+
+		// --- Descriptor set layout ---
+		void beginCreateDescriptorSetLayout()
+		{
+			m_curDescriptorSetInfo = {};
+			m_curSetLayoutName = static_cast<uint32_t>(m_descriptorSetLayouts.size());
+			m_descriptorSetLayouts[m_curSetLayoutName] = VDeleter<VkDescriptorSetLayout>{ m_device, vkDestroyDescriptorSetLayout };
+		}
+
+		void setLayoutAddBinding(uint32_t bindingPoint, VkDescriptorType type, VkShaderStageFlags shaderStages,
+			uint32_t count = 1, uint32_t immutableSamplerName = std::numeric_limits<uint32_t>::max())
+		{
+			m_curDescriptorSetInfo.bindings.push_back({});
+			VkDescriptorSetLayoutBinding &binding = m_curDescriptorSetInfo.bindings.back();
+
+			binding.binding = bindingPoint;
+			binding.descriptorType = type;
+			binding.descriptorCount = count;
+			binding.stageFlags = shaderStages;
+			binding.pImmutableSamplers = &m_samplers[immutableSamplerName];
+		}
+
+		uint32_t endCreateDescriptorSetLayout()
+		{
+			VkDescriptorSetLayoutCreateInfo layoutInfo = {};
+			layoutInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
+			layoutInfo.bindingCount = static_cast<uint32_t>(m_curDescriptorSetInfo.bindings.size());
+			layoutInfo.pBindings = m_curDescriptorSetInfo.bindings.data();
+
+			if (vkCreateDescriptorSetLayout(m_device, &layoutInfo, nullptr, m_descriptorSetLayouts[m_curSetLayoutName].replace()) != VK_SUCCESS)
+			{
+				throw std::runtime_error("failed to create descriptor set layout!");
+			}
+
+			m_curDescriptorSetInfo = {};
+			return m_curSetLayoutName;
+		}
+		// --- Descriptor set layout ---
 
 	protected:
 		void createPipelineCache()
@@ -209,6 +345,7 @@ namespace rj
 				throw std::runtime_error("failed to create pipeline cache.");
 			}
 		}
+
 
 #ifndef NDEBUG
 		bool m_enableValidationLayers = false;
@@ -223,8 +360,16 @@ namespace rj
 		VDeleter<VkPipelineCache> m_pipelineCache{ m_device, vkDestroyPipelineCache };
 
 		RenderPassCreateInfo m_curRenderPassInfo;
-		std::string m_curRenderPassName;
+		uint32_t m_curRenderPassName;
 		SubpassCreateInfo *m_pCurSubpassInfo;
-		std::unordered_map<std::string, VDeleter<VkRenderPass>> m_renderPasses;
+		std::unordered_map<uint32_t, VDeleter<VkRenderPass>> m_renderPasses;
+
+		DescriptorSetLayoutCreateInfo m_curDescriptorSetInfo;
+		uint32_t m_curSetLayoutName;
+		std::unordered_map<uint32_t, VDeleter<VkDescriptorSetLayout>> m_descriptorSetLayouts;
+
+
+
+		std::unordered_map<uint32_t, VDeleter<VkSampler>> m_samplers;
 	};
 }
